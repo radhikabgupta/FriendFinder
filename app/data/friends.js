3ecadbfafed13e0e3a1ecb8db0ae0d0f2bbbf552
radhikabgupta <33852730+radhikabgupta@users.noreply.github.com>
2018-05-12 09:08:21 -0400

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,42 +2,42 @@
 var friends = [
     {
         "name":"Owen Miller",
-        "photo":"https://radhikabgupta.github.io/FriendFinder/assets/images/mhs_001.jpg",
+        "photo":"https://radhikabgupta.github.io/FriendFinder/app/assets/images/mhs_001.jpg",
         "scores":[1, 2, 4, 2, 5, 2, 1, 3, 1, 2]
     },
     {
       "name": "Kimmy Schmidt",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/fmhs_001.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/fmhs_001.jpg",
       "scores": [4, 4, 2, 5, 3, 4, 5, 2, 3, 4]
     },    
     {
       "name": "Dexter Morgan",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/mhs_002.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/mhs_002.jpg",
       "scores": [3, 2, 5, 4, 1, 1, 4, 2, 3, 2]
     },
     {
       "name": "Maggie Rhee",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/fmhs_002.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/fmhs_002.jpg",
       "scores": [3, 2, 3, 1, 3, 2, 3, 1, 3, 5]
     },    
     {
       "name": "Sam Winchester",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/mhs_003.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/mhs_003.jpg",
       "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
       "name": "Emma Wilkins",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/fmhs_003.jpg",
-      "scores": [1, 4, 1, 4, 1, 4, 1, 2, 1, 4]
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/fmhs_003.jpg",
+      "scores": [1, 4, 5, 2, 1, 3, 1, 5, 2, 4]
     },      
     {
       "name": "Tom Winchester",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/mhs_004.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/mhs_004.jpg",
       "scores": [4, 5, 4, 5, 4, 5, 4, 5, 4, 5]
     },
     {
       "name": "Sue Wilkins",
-      "photo": "https://radhikabgupta.github.io/FriendFinder/assets/images/fmhs_004.jpg",
+      "photo": "https://radhikabgupta.github.io/FriendFinder/app/assets/images/fmhs_004.jpg",
       "scores": [1, 3, 1, 2, 4, 4, 1, 5, 5, 4]
     },
   ]
